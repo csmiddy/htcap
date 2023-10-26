60b6f727ffd1556409820c9d7d09150d7cc545a7
Chris Smiddy <chris.smiddy@gmail.com>
2023-10-25 23:04:58 -0500

    Update socks.py for Python latest 3.11.4
    Collections class moved in Python 3.3 this tries to load the new version and if not fallback to older Python

--- core/lib/thirdparty/pysocks/socks.py
+++ core/lib/thirdparty/pysocks/socks.py
@@ -59,7 +59,10 @@ import struct
 from errno import EOPNOTSUPP, EINVAL, EAGAIN
 from io import BytesIO
 from os import SEEK_CUR
-from collections import Callable
+try:
+    from collections.abc import Callable
+except ImportError:
+    from collections import Callable
 
 PROXY_TYPE_SOCKS4 = SOCKS4 = 1
 PROXY_TYPE_SOCKS5 = SOCKS5 = 2
